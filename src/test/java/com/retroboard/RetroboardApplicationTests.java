64316d7acbc88b973fa468358fb57dba8f3a4ad1
Vinayak Kamati <ervinayakkamati@gmail.com>
2023-07-24 09:44:12 +0530

    first commit

---
+++ src/test/java/com/retroboard/RetroboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retroboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
