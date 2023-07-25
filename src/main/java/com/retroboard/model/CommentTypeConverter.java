8f6c429196d4872ada4c6564e8ca1c41dfee1cec
Vinayak Kamati <119689320+vinayakkamati@users.noreply.github.com>
2023-07-25 22:47:45 +0530

    [IF-220] feat: configure liquibase and h2 database (#2)

---
+++ src/main/java/com/retroboard/model/CommentTypeConverter.java
@@ -0,0 +1,43 @@
+package com.retroboard.model;
+
+import com.retroboard.enums.CommentType;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CommentTypeConverter implements AttributeConverter<CommentType, Integer> {
+    @Override
+    public Integer convertToDatabaseColumn(CommentType type) {
+        switch (type){
+            case WENT_WELL:
+                return 1;
+
+            case TO_IMPROVE:
+                return 2;
+
+            case ACTION_ITEMS:
+                return 3;
+
+            default:
+                throw new IllegalArgumentException("Comment type" + type + "not supported");
+        }
+    }
+
+    @Override
+    public CommentType convertToEntityAttribute(Integer dbData) {
+             switch (dbData){
+                 case 1:
+                     return CommentType.TO_IMPROVE;
+
+                 case 2:
+                     return CommentType.WENT_WELL;
+
+                 case 3:
+                     return CommentType.ACTION_ITEMS;
+
+                 default:
+                     throw new IllegalArgumentException("Comment type" + dbData + "not supported");
+             }
+    }
+}
