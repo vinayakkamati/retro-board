750af4b45dd94f96ad07fde858a13df0e63eb872
Vinayak Kamati <ervinayakkamati@gmail.com>
2023-07-26 15:56:32 +0530

    [IF-220] feat: implement save comment

--- src/main/java/com/retroboard/RetroBoardApplication.java
+++ src/main/java/com/retroboard/RetroBoardApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RetroboardApplication {
+public class RetroBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RetroboardApplication.class, args);
+		SpringApplication.run(RetroBoardApplication.class, args);
 	}
-
 }
