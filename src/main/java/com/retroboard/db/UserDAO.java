0c3ab41064d926e373420efa85367c1bad1d3966
Vinayak Kamati <ervinayakkamati@gmail.com>
2023-07-27 15:11:14 +0530

    [IF-220] feature: implement update comment functionality

--- src/main/java/com/retroboard/db/UserDAO.java
+++ src/main/java/com/retroboard/db/UserDAO.java
@@ -2,6 +2,8 @@ package com.retroboard.db;
 
 import com.retroboard.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDAO extends JpaRepository<UserEntity,Long> {
 }
--- src/main/java/com/retroboard/services/CommentService.java
+++ src/main/java/com/retroboard/services/CommentService.java
@@ -1,14 +1,18 @@
 package com.retroboard.services;
 
+import com.retroboard.dtos.CommentDTO;
 import com.retroboard.entities.CommentEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentService {
-    void saveComment(CommentEntity commentEntity);
-    Iterable<CommentEntity> findAll();
+    void saveComment(CommentDTO commentDTO);
+    List<CommentDTO> findAll();
 
     void deleteComment(Long id);
 
     Optional<CommentEntity> findById(Long id);
+
+    void updateComment(Long id, String commentDescription);
 }
